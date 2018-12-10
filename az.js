5cdc4b1706bad4d23b5f78c017e3670be4eddf5b
bryanbultot <darkoni59610@gmail.com>
2018-12-10 22:14:48 +0100

    cv fini presque

---
+++ az.js
@@ -0,0 +1,17 @@
+$('#mabio').click(function () {
+
+    $('#qui').slideToggle('#qui');
+
+
+
+
+
+
+});
+$('#parcour').click(function () {
+    $('#parpar').slideToggle('#parpar')
+});
+$('#dip').click(function () {
+    $('#container3').slideToggle('#container3')
+
+});
